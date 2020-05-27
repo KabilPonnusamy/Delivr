8e5d16b62e8ea4a289a35a88c7e9b69128a2f34a
NathiyaKumar <nathiyakumar8087@gmail.com>
2020-05-27 11:07:56 +0530

    push notification azure hub updates

---
+++ app/src/main/java/com/delivr/pushmessaging/NotificationSettings.java
@@ -0,0 +1,6 @@
+package com.delivr.pushmessaging;
+
+public class NotificationSettings {
+    public static String HubName = "<your hub's name>";
+    public static String HubListenConnectionString = "<your hub's listen endpoint>";
+}
