b5df1a879360825bcdc961bafb175a00aadbd352
ADMIN <kabil444dev@gmail.com>
2020-06-04 18:32:52 +0530

    May_30 Location Done

---
+++ app/src/main/java/sg/delivr/ui/adapters/SearchProduct.java
@@ -0,0 +1,94 @@
+package sg.delivr.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sg.delivr.R;
+import sg.delivr.ui.models.SearchDatas;
+
+public class SearchProduct extends ArrayAdapter<SearchDatas> {
+
+    Context context;
+    int resource, textViewResourceId;
+    List<SearchDatas> items, tempItems, suggestions;
+
+    public SearchProduct(Context context, int resource, int textViewResourceId, List<SearchDatas> items) {
+        super(context, resource, textViewResourceId, items);
+        this.context = context;
+        this.resource = resource;
+        this.textViewResourceId = textViewResourceId;
+        this.items = items;
+        tempItems = new ArrayList<SearchDatas>(items); // this makes the difference.
+        suggestions = new ArrayList<SearchDatas>();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.ly_search_item, parent, false);
+        }
+        SearchDatas people = items.get(position);
+        if (people != null) {
+            TextView lblName = (TextView) view.findViewById(R.id.service_name);
+            if (lblName != null)
+                lblName.setText(people.getLoc_name());
+        }
+        return view;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return nameFilter;
+    }
+
+    /**
+     * Custom Filter implementation for custom suggestions we provide.
+     */
+    Filter nameFilter = new Filter() {
+        @Override
+        public CharSequence convertResultToString(Object resultValue) {
+            String str = ((SearchDatas) resultValue).getLoc_name();
+            return str;
+        }
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            if (constraint != null) {
+                suggestions.clear();
+                for (SearchDatas people : tempItems) {
+                    if (people.getLoc_name().toLowerCase().contains(constraint.toString().toLowerCase())) {
+                        suggestions.add(people);
+                    }
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = suggestions;
+                filterResults.count = suggestions.size();
+                return filterResults;
+            } else {
+                return new FilterResults();
+            }
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            List<SearchDatas> filterList = (ArrayList<SearchDatas>) results.values;
+            if (results != null && results.count > 0) {
+                clear();
+                for (SearchDatas people : filterList) {
+                    add(people);
+                    notifyDataSetChanged();
+                }
+            }
+        }
+    };
+}
--- app/src/main/java/sg/delivr/ui/fragments/Frag_MyOrders.java
+++ app/src/main/java/sg/delivr/ui/fragments/Frag_MyOrders.java
@@ -32,6 +32,7 @@ import sg.delivr.backend.RetrofitClient;
 import sg.delivr.backend.postmodels.PostDoRiderQueue;
 import sg.delivr.backend.responsemodels.ResponseRiderQueue;
 import sg.delivr.data.model.MyJobsModel;
+import sg.delivr.ui.activity.SearchLocation;
 import sg.delivr.ui.adapters.MyJobsAdapter;
 import sg.delivr.ui.interfaces.Intent_Constants;
 import sg.delivr.ui.interfaces.SHAInterface;
@@ -57,6 +58,8 @@ public class Frag_MyOrders extends Fragment implements View.OnClickListener, SHA
     TextView label_empty_myjobs;*/
     //BottomNavigationView bottom_nav;
 
+    TextView txt_address;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -69,93 +72,17 @@ public class Frag_MyOrders extends Fragment implements View.OnClickListener, SHA
         View view = inflater.inflate(R.layout.frag_myorders, container, false);
 
         toolbarInit();
-//        initView(view);
+        initView(view);
 //        initiateProgress();
 
         return view;
-        }
-
-    /*private void initiateProgress() {
-        progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setMessage("Please wait..");
-        progressDialog.setCancelable(false);
-
-        if (!CheckNetwork.isInternetAvailable(getActivity()))  //if connection available
-        {
-            AlertBox(getResources().getString(R.string.error), getResources().getString(R.string.network));
-        }
-
-        gatherRiders();
-    }
-
-    private void gatherRiders() {
-        String Strapikey = getString(R.string.apikey);
-        String Strapicode = getString(R.string.apicode);
-        String sign =  userId + Strapikey + Strapicode;
-        String StrSignature = SHAInterface.SHA1(sign);
-        Log.e("delivrApp", "Signature: " + StrSignature);
-        Log.e("delivrApp", "Sign: " + sign);
-        Log.e("delivrApp", "StraipKey: " + Strapikey);
-        Log.e("delivrApp", "StraipCode: " + Strapicode);
-
-        progressDialog.show();
-        callRiderQueue = RetrofitClient.getInstance().getApiInterface().getRiderQueue(
-                new PostDoRiderQueue( userId, Strapikey, StrSignature));
-
-        callRiderQueue.enqueue(new Callback<ArrayList<ResponseRiderQueue>>() {
-            @Override
-            public void onResponse(Call<ArrayList<ResponseRiderQueue>> call,
-                                   final Response<ArrayList<ResponseRiderQueue>> response) {
-                progressDialog.dismiss();
-                if (response.body() != null) {
-                    riderQueues = response.body();
-                    showAdapters();
-                } else {
-                    Utils.showMessageDialog(getActivity(),
-                            getString(R.string.dialog_title_sorry),
-                            "No Data found");
-                }
-            }
-
-
-            @Override
-            public void onFailure(Call<ArrayList<ResponseRiderQueue>> call, Throwable t) {
-                progressDialog.dismiss();
-                t.printStackTrace();
-                Prefs.setLoginVerified("failure");
-                Utils.showGenericErrorDialog(getActivity());
-            }
-        });
-
-    }
-
-    private void showAdapters() {
-        if(riderQueues != null) {
-            if(riderQueues.size() > 0) {
-                jobs_recycler.setVisibility(View.VISIBLE);
-                label_empty_myjobs.setVisibility(View.GONE);
-                jobs_recycler.setHasFixedSize(true);
-                jobs_recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
-                myjobsAdapter = new MyJobsAdapter(riderQueues, getActivity(),
-                        Frag_MyOrders.this);
-                jobs_recycler.setAdapter(myjobsAdapter);
-
-            } else {
-                jobs_recycler.setVisibility(View.GONE);
-                label_empty_myjobs.setVisibility(View.VISIBLE);
-            }
-        }
     }
 
     private void initView(View view) {
-        userId = Prefs.getUserId();
+        txt_address = view.findViewById(R.id.txt_address);
+        txt_address.setOnClickListener(this);
+    }
 
-        riderQueues = new ArrayList<ResponseRiderQueue>();
-        jobs_recycler = view.findViewById(R.id.jobs_recycler);
-        label_empty_myjobs = view.findViewById(R.id.label_empty_myjobs);
-        *//*bottom_nav = getActivity().findViewById(R.id.bottom_nav);
-        bottom_nav.setVisibility(View.VISIBLE);*//*
-    }*/
 
     private void toolbarInit() {
         dash_toolbar = getActivity().findViewById(R.id.dash_toolbar);
@@ -167,86 +94,23 @@ public class Frag_MyOrders extends Fragment implements View.OnClickListener, SHA
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.txt_address:
+                Intent intent = new Intent(getActivity(), SearchLocation.class);
+                startActivityForResult(intent, FRAG_MYORDERS_to_SEARCH_LOCATION);
+                break;
         }
     }
 
-    /*public void AlertBox(final String head, final String meg) {
-        LayoutInflater in = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View vv = in.inflate(R.layout.alertbox, null);
-        final BottomSheetDialog dialog = new BottomSheetDialog(getActivity());
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(vv);
-        dialog.setCancelable(false);
-        TextView content = (TextView) vv.findViewById(R.id.content);
-        TextView header = (TextView) vv.findViewById(R.id.header);
-        header.setText(head);
-        TextView no = (TextView) vv.findViewById(R.id.no);
-        TextView yes = (TextView) vv.findViewById(R.id.yes);
-        LinearLayout cancel = (LinearLayout) vv.findViewById(R.id.cancel);
-        cancel.setVisibility(View.GONE);
-        LinearLayout ok = (LinearLayout) vv.findViewById(R.id.ok);
-        content.setText(meg);
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
 
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-                if (head.equalsIgnoreCase("Error")) {
-                    getActivity().finish();
-                } else if (head.equalsIgnoreCase("Alert")) {
-                    dialog.dismiss();
-                } else {
-                    dialog.dismiss();
-                }
-            }
-        });
-        dialog.show();
-    }*/
-
-    /*@Override
+    @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == RIDER_MYJOBS_to_MYJOB_DETAILS) {
-            if(resultCode == MYJOBSAQ_success) {
-                String listAQPos = data.getStringExtra("AQPosition");
-                String jobstatus = data.getStringExtra("AQStatus");
-                int position = Integer.parseInt(listAQPos);
-                Log.e("delivrApp", "LastPos: " + position  + "Job Status:" +  jobstatus);
-
-                if (jobstatus.equalsIgnoreCase("Del")) {
-                    riderQueues.remove(position);
-                    myjobsAdapter.notifyDataSetChanged();
-                } else {
-                    String chkstatus = "";
-                    progressDialog = new ProgressDialog(getActivity());
-                    progressDialog.setMessage("Please wait..");
-                    progressDialog.setCancelable(false);
-                    if (jobstatus.equalsIgnoreCase("Pic")) {
-                        gatherRiders();
-                    }
-                    riderQueues.get(position).setStatus(jobstatus);
-                    StoredDatas.getInstance().setRiderQueues(riderQueues);
-                    riderQueues = StoredDatas.getInstance().getRiderQueues();
-                    Log.e("delivrApp", "Inside My Jobs get current data : Check Status:" + chkstatus + "New Status:"  + riderQueues.get(position).getStatus()+
-                            "Status Code:" +riderQueues.get(position).getStatusCode() + "Delivery_CO" + riderQueues.get(position).getDelivery_CO());
-                    if (riderQueues.get(position).getStatusCode().equals("RidAcc") || riderQueues.get(position).getStatusCode().equals("PassAcc")
-                            || riderQueues.get(position).getStatusCode().equals("PassRej")) {
-                        chkstatus = riderQueues.get(position).getLastStatusCode();
-                    } else {
-                        chkstatus = riderQueues.get(position).getStatusCode();
-                    }
-                    Log.e("delivrApp", "Inside My Jobs else : Check Status:" + chkstatus + "New Statusd:"  + riderQueues.get(position).getStatus() + "Status Code:" +
-                            riderQueues.get(position).getLastStatusCode() + "Delivery_CO" + riderQueues.get(position).getDelivery_CO());
-                    myjobsAdapter.notifyDataSetChanged();
-                }
+        if(requestCode == FRAG_MYORDERS_to_SEARCH_LOCATION) {
+            if(resultCode == SEARCH_success) {
+                String location_value = data.getStringExtra("location_value");
+                Log.e("delivrApp", "Location: " + location_value);
+                txt_address.setText(location_value);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
-    }*/
+    }
 }
